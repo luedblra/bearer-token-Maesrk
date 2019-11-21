af88aaa7af5b7551056a69d33ec451df2b4a02bd
Luis <luedblra@gmail.com>
2019-11-21 11:35:05 -0400

    se guarda avance

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer');
 const CREDS     = require('./creds');
 const fs        = require('fs').promises;
+"use strict";
+
+const http = require("http");
+const util = require("util");
 
 async function run() {
     const browser = await puppeteer.launch({headless: false,args: ['--no-sandbox', '--disable-setuid-sandbox']});
@@ -22,29 +26,29 @@ async function run() {
 
     await page.click(PASSWORD_SELECTOR);
     await page.keyboard.type(CREDS.password);
-    console.log(CREDS.username);
-    console.log(CREDS.password);
+    //console.log(CREDS.username);
+    //console.log(CREDS.password);
     //await page.screenshot({ path: 'screenshots/github.png' });
     const BUTTON_SELECTOR = '#login-form > fieldset > div:nth-child(4) > button';
     await page.click(BUTTON_SELECTOR);
     await page.waitForNavigation();
-    let response = await page.goto('https://www.maersk.com/instantPrice/');
-    let headerPage = '';
-    await page.on('response', response => {
+
+    const response = await page.goto('https://www.maersk.com/instantPrice/');
+
+    page.on('response', response => {
         //console.log('Response Request:', response.request());
         const req = response.request();
         //console.log('Response URl:', req.url());
         if (req.url() === 'https://api.maersk.com/tokenValidation?serviceName=product-prices') {
             response.buffer().then(
                 b => {
-                    headerPage = JSON.stringify(req.headers());
-                    let inicio = headerPage.indexOf('Bearer');
-                    let fin = headerPage.length;
-                    let texto;
-                    texto  = headerPage.substring(inicio,fin-2);
-                    headerPage = texto;
-                    console.log(texto);
-            
+                    let headerPage  = '';
+                    headerPage      = JSON.stringify(req.headers());
+                    let inicio      = headerPage.indexOf('Bearer');
+                    let fin         = headerPage.length;
+                    let bearer_enc  = headerPage.substring(inicio,fin-2);
+                    //console.log(bearer_enc);
+                    extrae(bearer_enc);
                 },
                 e => {
                     console.error(`${response.status()} ${response.url()} failed: ${e}`);
@@ -53,14 +57,23 @@ async function run() {
         }
     });
 
+    await page.waitFor(2000);
+
+    //console.log('llega');
+    await page.goto(`https://www.maersk.com/portaluser/logoff`);
 
     //console.log(texto2);
-    await page.waitForNavigation();
-
-    await page.goto(`https://www.maersk.com/portaluser/logoff`);
     await page.close();
 
     browser.close();
 }
+  
+function extrae(data){
+    http.createServer((request, response) => {
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.end(util.inspect(data));
+    }).listen(8000, "::1");
+}
+
 
 run();
